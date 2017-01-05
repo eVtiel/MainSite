644798f40f98db1485d88989930cddc02425b7ed
Alex Shapovalov <evtiel.j@gmail.com>
2017-01-05 05:58:35 -0500

    Added resume link and new projects

--- js/common.js
+++ js/common.js
@@ -1,9 +1,21 @@
-$(window).scroll(function(){
-	var st = $(this).scrollTop();
+// $(window).scroll(function(){
+// 	var st = $(this).scrollTop();
 
-	$('.container-fluid').css({
-		'transform' : 'translate(0%, -'+ st /20 + '%'
-	});
+// 	$('.container-fluid').css({
+// 		'transform' : 'translate(0%, -'+ st /20 + '%'
+// 	});
 
 
+// });
+
+jQuery(window).resize(function() {
+  if (jQuery(window).width() < 768) {
+    jQuery(".footer1, .footer2").css('height', '500');
+  }
+});
+
+jQuery(window).resize(function() {
+  if (jQuery(window).width() > 768) {
+    jQuery(".footer1, .footer2").css('height', '684');
+  }
 });
